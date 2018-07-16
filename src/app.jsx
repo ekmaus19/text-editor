28633f0123bea7db545c1bcebb1bc6116bb048f2
Jaroslav-Tran <jaroslav.tran@gmail.com>
2018-07-16 16:10:52 -0700

    Adding Login and Reggistration Components

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
+import Login from './comp/Login'
+import Registration from './comp/Registration'
 export default class App extends React.Component {
   render() {
     return (<div>
-      <h2>Welcome to React!</h2>
+      <h2>Hello World!</h2>
+        <Login />
     </div>);
   }
 }
