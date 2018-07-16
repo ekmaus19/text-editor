0cde30d62281db053c811e0459c50847d949ad13
Jaroslav-Tran <jaroslav.tran@gmail.com>
2018-07-16 16:27:42 -0700

    Updated styling of Log in and Registration components

--- src/app.jsx
+++ src/app.jsx
@@ -5,8 +5,7 @@ import Registration from './comp/Registration'
 export default class App extends React.Component {
   render() {
     return (<div>
-      <h2>Hello World!</h2>
-        <Login />
+      <Login />
     </div>);
   }
 }
--- src/comp/Login.js
+++ src/comp/Login.js
@@ -1,23 +1,60 @@
-import React from 'react'
-import { Button, Form, Grid, Header, Image, Message, Segment, Checkbox} from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
 
-const FormExampleForm = () => (
-    <Form>
-        <Form.Field>
-            <label>Username</label>
-            <input placeholder='Username' />
-        </Form.Field>
-        <Form.Field>
-            <label>Password</label>
-            <input type="password" placeholder='Password' />
-        </Form.Field>
-        <Form.Field>
-            <Checkbox label='I agree to the Terms and Conditions' />
-        </Form.Field>
-        <Button type='submit'>Log In</Button>
-        <Button type='submit'>Sign Up</Button>
-    </Form>
-)
+    render() {
+        return (
+            <div className='login-form'>
+                {/*
+          Heads up! The styles below are necessary for the correct render of this example.
+          You can do same with CSS, the main idea is that all the elements up to the `Grid`
+          below must have a height of 100%.
+        */}
+                <style>{`
+          body > div,
+          body > div > div,
+          body > div > div > div.login-form {
+            height: 100%;
+          }
+        `}</style>
+                <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                        <Header as='h2' color='purple' textAlign='center'>
+                            Log In
+                        </Header>
+                        <Form size='large'>
+                            <Segment stacked>
+                                <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                                <Form.Input
+                                    fluid
+                                    icon='lock'
+                                    iconPosition='left'
+                                    placeholder='Password'
+                                    type='password'
+                                />
 
-export default FormExampleForm
+                                <Button color='purple' fluid size='large'>
+                                    Login
+                                </Button>
+                            </Segment>
+                        </Form>
+                        <Message>
+                            New user? <a href='#'>Sign Up</a>
+                        </Message>
+                        <Message><a href='#'>Cancel</a></Message>
+                    </Grid.Column>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default Login
